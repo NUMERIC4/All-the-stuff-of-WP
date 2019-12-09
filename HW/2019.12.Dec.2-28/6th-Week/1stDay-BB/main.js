090a214fd4f679d6cfe3eccd2dfc32aaa26a6483
NUMERIC4 <ahmet.karan@users.noreply.github.com>
2019-12-09 21:01:59 +0100

    .

--- HW/2019.12.Dec.2-28/6th-Week/1stDay-BB/main.js
+++ HW/2019.12.Dec.2-28/6th-Week/1stDay-BB/main.js
@@ -1,2 +1,32 @@
+//comment dediniz yazdık
 console.log('bullss');
 console.log('Bulls2');
+alert("nope world");
+
+var myFirstVar;
+myFirstVar = 'Hello World Var';
+//js de düşünce tarzımızı değiştirmemiz gerekiyor. Biraz daha vücuttaki sinirler gibi bağlantılı yapıda bulunmakta ve bu bağlantıları kurabilme yetisi gerekiyor
+
+alert(myFirstVar);
+
+//This line changes next console log data
+//myFirstVar = 13;
+console.log(myFirstVar);
+
+
+myFirstVar = 13;
+
+console.log(myFirstVar);
+
+var mySecVar = '57';
+
+var total = (myFirstVar + mySecVar);
+
+console.log(total);
+
+
+var myText1 = ('Chikilata');
+var myText2 = ('uff bea');
+
+//içerdeki tırnak iki string arasına bir boşluk bırakmak için
+console.log(myText1 + ' ' + myText2);
