158a0a07bd1b27bf4adfc74b59b8d8b139e51418
NUMERIC4 <ahmet.karan@users.noreply.github.com>
2019-12-10 18:53:01 +0100

    .

---
+++ HW/2019.12.Dec.2-28/6th-Week/2ndDay-BB/main.js
@@ -0,0 +1,54 @@
+Boolean
+
+let myFirstBool = false; //burada true dendiği için boolean tipine geçiş yapıldı
+//let myFirstBool; //bu tanımda sınıfı any
+// if (myFirstBool == true){
+//         alert('ahanda doğru');
+// }
+// else{alert('Anaa yanlışmış');}
+
+
+
+
+// var myHelYeahVariable = 12;
+
+// if (myHelYeahVariable != false) {
+//     alert('1')
+// }
+// else {alert('2');}
+
+// if (myHelYeahVariable == false){
+//     alert('3')
+// }
+// else {alert('4');}
+
+
+// if (myHelYeahVariable != true) {
+//     alert('1')
+// }
+// else {alert('2');}
+
+// if (myHelYeahVariable == true){
+//     alert('3')
+// }
+// else {alert('4');}
+myFirstBool = true;
+
+//boolean and if statement equal (==)
+if (myFirstBool = true) {
+    let x = 13;
+    alert('1');
+    console.log('2');
+}
+myFirstBool = false;
+//boolean and if statemnet with not equal (!=)
+if (myFirstBool != true) {
+    alert('3');
+    console.log('4');
+}
+//results are same on previous and next samples
+myFirstBool = 15;
+if (myFirstBool != true) {
+    alert('5');
+    console.log('6');
+}
