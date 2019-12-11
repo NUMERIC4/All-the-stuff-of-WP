be96d9cd0525ab074b2f4772f65af4eb472a0d55
NUMERIC4 <ahmet.karan@users.noreply.github.com>
2019-12-11 20:57:37 +0100

    .

--- HW/2019.12.Dec.2-28/6th-Week/3rdDay/main.js
+++ HW/2019.12.Dec.2-28/6th-Week/3rdDay/main.js
@@ -52,9 +52,157 @@ console.log(y[1]);              //burada array inide de değişken tanımlanabil
 */
 //*********yanımlayamatyız dedi array içinde neden? */
 //buradan for loop a giricez
+
+//loop types
+//--------
+//1. For
+// 2. For/IN => objects
+// 3. FOR / OF => ARRAYS
+// 4. WHILE
+// 5. DO / WHILE
+
 var x = ['hasan','ahmet','veli'];
-console.log(x[0]);
+
+/*console.log(x[0]);
 console.log(x[1]);
 console.log(x[2]);
+*/
+/*
+//for (1,2,3){4;5;...}      1 değişkenimiz sayacımız 2 statement 3 statement true iken olacak değişim 4 ve 5 her true durumunda alınıcak aksiyon
+for (var i = 0 ; i < x.length ; i++){
+    console.log(x[i]);
+}
 
-for[]
+var i;
+for (i=0;i <x.length;i++){
+    console.log(x[i])
+}
+*/
+/*
+var array = [];
+var num1 = window.prompt("Lütfen 1sayı gir");
+array.push(parseInt(num1));     //parseInt ile data type değiştiriyoruz diğer türlü string kabul ediliyor
+var num2 = window.prompt("Lütfen 2sayı gir");
+array.push(parseInt(num2));
+// console.log(num1);
+if (array[0] > array[1])
+{console.log("girdiğiniz iki rakamdan büyük olan aha bu : " + array[0]);}
+else if (array[1] > array[0])
+{console.log("girdiğiniz iki rakamdan büyük olan aha bu : " + array[1]);}
+else
+{console.log("sayılar eşit");}
+console.log(array);
+
+*/
+/*
+// 0 dan 15 e tek olan sayıları bastır
+var f = 0;
+for(var i = 0; i<=15; i++)
+{
+    if (f==0)
+    {
+        
+        f=1;
+    }
+    else{
+        console.log(i);
+        f=0;
+    }
+}
+*/
+/*
+//sayının tek mi çift mi olduğunu belirleyecek döngü
+for ( var elem = 0; elem <=15; elem++){
+
+    if ( elem === 0 ){
+        console.log(`${elem} çifttir.`);
+    } else if ( elem % 2 === 0) {
+        console.log(`${elem} çifttir anam`);
+    } else {
+        console.log(`${elem} tektir`);
+    }
+}*/
+/*
+for ( var elem = 0; elem <=15; elem++){
+
+    if ( elem === 0 ){
+        console.log(`${elem} çifttir.`);
+    } else if ( elem % 2 !== 0) {
+        console.log(`${elem} tekttir anam`);
+    } else {
+        console.log(`${elem} çifttir gülüm`);
+    }
+}
+*/
+/*
+for ( var elem = 0; elem <=15; elem++){
+
+    if ( elem % 2 !== 0 ){
+        console.log(`${elem} tekrtir.`); //elemin içinde bulunduğu kısım tırnak değil bag tick
+    } else if ( elem % 2 === 0) {
+        console.log(`${elem} çifttir anam`);
+    } else {
+        console.log(`${elem} tektir`);
+    }
+}
+*/
+
+//3 e bölünen fiz 5e bölünen buz ikisinede bölünen fiz buz yazıcak
+//benim çözümüm
+/*
+for ( var t = 1 ; t<101 ; t++){
+    if (t % 3 == 0 ){
+        if(t % 5== 0){
+            console.log(`${t}fiz buz`);
+        }
+        else{
+            console.log(`${t}fiz`);
+        }
+    }
+    else{
+        if(t % 5 == 0){
+            console.log(`${t}buz`);
+        }
+    }
+}
+*/
+
+//hocanın çözümü
+/*
+for ( var t = 1 ; t<101; t++){
+    if ( t % 3 === 0 && t % 5 === 0){
+        console.log(`${t} FizzBuz`);
+    }
+    else if (t % 5 === 0){
+        console.log(`${t}buz`);
+    }
+    else if (t%3===0){
+        console.log(`${t}fizz`)
+    }
+    else{
+        console.log(t);
+    }
+}
+*/
+//arraye sokma
+
+var y = [];
+for ( var t = 1 ; t<101; t++){
+    if ( t % 3 === 0 && t % 5 === 0){
+        console.log(`${t} FizzBuz`);
+        y.push([t,'Fizzbuz']);
+    }
+    else if (t % 5 === 0){
+        console.log(`${t}buz`);
+        y.push([t,'Fizz']);
+    }
+    else if (t%3===0){
+        console.log(`${t}fizz`);
+        y.push([t,'Buz']);
+    }
+    else{
+        console.log(t);
+    }
+}
+
+console.log(y);
